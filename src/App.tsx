77608b3d93fa14356a16a7a204bb2b39eaf17f30
LucasSantosp09 <lucassantos.p09@gmail.com>
2024-01-21 17:34:36 -0300

    Styled Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
-import { useState } from "react";
+import { Button } from "./components/Button";
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/themes/default";
 
 export function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <ThemeProvider theme={defaultTheme}>
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="success" />
+      <Button variant="danger" />
+      <Button />
+    </ThemeProvider>
   );
 }
